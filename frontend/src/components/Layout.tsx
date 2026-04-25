25b0247eb2375c774e65a59d47a232fad2ddaa0a
sker101 <georgekenneth1010@gmail.com>
2026-04-25 05:44:06 +0300

    Fix eslint errors to unblock CI build

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -199,7 +199,6 @@ export default function Layout({ children, hideSidebar = false, hideHeader = fal
       window.removeEventListener('resize', handleResize);
       document.removeEventListener('click', handleOutside);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Close mobile drawer on route change
